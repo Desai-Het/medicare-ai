848b2dd158acb9030744ccf86d7c8391078ad3c1
HD <hetdesaius@gmail.com>
2026-03-09 19:55:27 -0500

    Restore setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='MediCare-AI',
+    version='0.1.0',
+    author='Het Desai',
+    author_email='hetdesaius@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    )
